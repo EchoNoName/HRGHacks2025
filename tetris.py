b71903640cec5e23111ed03b1c7b3fab70053a34
EchoNoName <gu.ethan88@gmail.com>
2025-02-18 15:11:58 -0500

    Added some inputs

--- tetris.py
+++ tetris.py
@@ -1,9 +1,11 @@
 import pygame
 import random
+import os
 
 class Tetris:
     def __init__(self, screen, width = 10, height = 22, tile_pool = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']):
         self.screen = screen
+        self.tetris_board = pygame.image.load(os.join('assets', 'sprites', 'BoardWithoutBorder.png'))
         self.width = width
         self.height = height
         self.board = []
@@ -19,8 +21,12 @@ class Tetris:
     def draw_board(self):
         x = 200
         y = 50
+        self.screen.blit(self.tetris_board, (x, y))
         for row in reversed(self.board):
-            for 
+            for tile in row:
+                tile.draw(x, y)
+                x += 39
+            y += 40
     
     def start_game(self, grav, fast):
         running = True
@@ -28,14 +34,19 @@ class Tetris:
         random.shuffle(self.next_tile_pool)
         pygame.init()
         self.clock = pygame.time.Clock()
-        holding = None
+        self.holding = None
         grav_tick = grav
         fast_drop_tick = fast
         fast_drop = False
         current_tick = 0
+        shift_tick = 0
         self.spawn_block()
         defeat = False
         locking = False
+        shifting_right = False
+        shifting_left = False
+        shift_start_tick = 0
+        held = False
         while running:
             self.clock.tick(60)
             current_tick += 1
@@ -45,11 +56,13 @@ class Tetris:
                     for line in clears:
                         self.clear_line(self.board[line])
                 defeat = self.spawn_block()
+                held = False
                 if defeat == True:
                     break
             else:
                 if locking:
                     self.active_piece.lock -= 1
+                    current_tick = 0
                     if self.active_piece.lock == 0:
                         self.active_piece = None
                 elif fast_drop:
@@ -62,7 +75,83 @@ class Tetris:
                         current_tick = 0
                         self.active_piece.drop(self.board)
                         locking = self.active_piece.lock_check(self.board)
-                        
+                
+                if shifting_left:
+                    shift_start_tick += 1
+                    if shift_start_tick == 16:
+                        self.active_piece.shift_left(self.board)
+                        locking = self.active_piece.lock_check(self.board)
+                    elif shift_start_tick > 16:
+                        shift_tick += 1
+                        if shift_tick == 6:
+                            self.active_piece.shift_left(self.board)
+                            locking = self.active_piece.lock_check(self.board)
+                            shift_tick = 0
+
+                elif shifting_right:
+                    shift_start_tick += 1
+                    if shift_start_tick == 16:
+                        self.active_piece.shift_right(self.board)
+                        locking = self.active_piece.lock_check(self.board)
+                    elif shift_start_tick > 16:
+                        shift_tick += 1
+                        if shift_tick == 6:
+                            self.active_piece.shift_right(self.board)
+                            locking = self.active_piece.lock_check(self.board)
+                            shift_tick = 0
+            
+            events = pygame.event.get()
+            for event in events:
+                if event == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_F4 and (event.mod & pygame.KMOD_ALT):
+                    running = False
+                if event.key == pygame.K_s:
+                    if event.type == pygame.KEYDOWN:
+                        if self.active_piece:
+                            if not locking:
+                                self.active_piece.drop(self.board)
+                                locking = self.active_piece.lock_check(self.board)
+                                fast_drop = True
+                    elif event.type == pygame.KEYUP:
+                        fast_drop = False
+                elif event.key == pygame.K_c and event.type == pygame.KEYDOWN:
+                    if not held:
+                        self.hold()
+                        held = True
+                elif event.key == pygame.K_a:
+                    if self.active_piece:
+                        if self.event.type == pygame.KEYDOWN:
+                            self.active_piece.shift_left(self.board)
+                            locking = self.active_piece.lock_check(self.board)
+                            shifting_left = True
+                        elif event.type == pygame.KEYUP:
+                            shifting_left = False
+                            shift_tick = 0
+                            shift_start_tick = 0
+                elif event.key == pygame.K_d:
+                    if self.active_piece:
+                        if self.event.type == pygame.KEYDOWN:
+                            self.active_piece.shift_right(self.board)
+                            locking = self.active_piece.lock_check(self.board)
+                            shifting_right = True
+                        elif event.type == pygame.KEYUP:
+                            shifting_right = False
+                            shift_tick = 0
+                            shift_start_tick = 0
+
+    def hold(self):
+        if self.holding:
+            spawn = self.holding
+            self.holding = self.active_piece.piece_type
+            for tile in self.active_piece.occupied_tiles:
+                tile.unoccupie()
+            self.spawn_block(spawn)
+        else:
+            self.holding = self.active_piece.piece_type
+            for tile in self.active_piece.occupied_tiles:
+                tile.unoccupie()
+            self.spawn_block()
 
     def clear_line(self, line):
         for tile in line:
@@ -84,8 +173,10 @@ class Tetris:
         self.next_tile_pool = self.base_tile_pool
         random.shuffle(self.next_tile_pool)
 
-    def spawn_block(self):
-        if self.current_tile_pool:
+    def spawn_block(self, hold = False):
+        if hold:
+            self.active_piece = Block(hold)
+        elif self.current_tile_pool:
             self.active_piece = Block(self.current_tile_pool[-1])
             self.current_tile_pool.pop(-1)
         else:
@@ -204,6 +295,22 @@ class Tile:
         self.block_in_tile = None
         self.x = x
         self.y = y
+        self.red = pygame.image.load(os.join('assets', 'sprites', 'SingleTileRed'))
+        self.blue = pygame.image.load(os.join('assets', 'sprites', 'SingleTileBlue'))
+        self.cyan = pygame.image.load(os.join('assets', 'sprites', 'SingleTileCyan'))
+        self.green = pygame.image.load(os.join('assets', 'sprites', 'SingleTileGreen'))
+        self.orange = pygame.image.load(os.join('assets', 'sprites', 'SingleTileOrange'))
+        self.purple = pygame.image.load(os.join('assets', 'sprites', 'SingleTilePurple'))
+        self.yellow = pygame.image.load(os.join('assets', 'sprites', 'SingleTileYellow'))
+        self.colours = {
+            'O': self.yellow, 
+            'I': self.cyan,
+            'T': self.purple, 
+            'L': self.orange,
+            'J': self.blue,
+            'Z': self.red,
+            'S': self.green
+        }
     
     def occupie(self, block: Block):
         self.block_in_tile = block
@@ -221,9 +328,11 @@ class Tile:
     def isEmpty(self):
         return self.state == ' '
 
-    def draw(self):
+    def draw(self, screen, x ,y):
         if self.state != ' ':
             return True
+        else:
+            screen.blit(self.colours[self.state], (x, y))
 
     def __str__(self):
         return self.state
